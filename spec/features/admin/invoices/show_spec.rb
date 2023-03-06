64242f607fc7a4b6d02b76aa83b02ebaa1f0018a
Caleb Thomas <cjthomas00@comcast.net>
2023-03-06 15:36:26 -0700

    write test for US8.

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -16,7 +16,7 @@ describe 'Admin Invoices Index Page' do
     @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 12, unit_price: 2, status: 0)
     @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 6, unit_price: 1, status: 1)
     @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_2.id, quantity: 87, unit_price: 12, status: 2)
-
+    @bulk_discount_1 = BulkDiscount.create!(percentage_discount: 10, quantity_threshold: 10, merchant_id: @m1.id)
     visit admin_invoice_path(@i1)
   end
 
@@ -68,5 +68,11 @@ describe 'Admin Invoices Index Page' do
       expect(current_path).to eq(admin_invoice_path(@i1))
       expect(@i1.status).to eq('completed')
     end
+  end
+
+  #user story 8
+  it "When I visit an admin invoice show page Then I see the total revenue from this invoice (not including discounts) And I see the total discounted revenue from this invoice which includes bulk discounts in the calculation" do
+    expect(page).to have_content("Total Revenue: $#{@i1.total_revenue}")
+    expect(page).to have_content("Total Discounted Revenue: $#{@i1.discounted_revenue.round(2)}")
   end
 end
